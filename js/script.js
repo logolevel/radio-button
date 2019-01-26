67f8a99258e17006fcab529e6060a706bd72e831
logolevel <logolevel@gmail.com>
2019-01-26 11:25:34 +0200

    Add temp choice

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 'use strict'
 
 var showChoiceBtn = document.getElementById('showChoise'),
-    inputRadioCollection = document.getElementsByName('radio-group');
+    inputRadioCollection = document.getElementsByName('radio-group'),
+    tempChoiceOutput = document.getElementById('show-temp-choice');
 
-console.log(inputRadioCollection);
+
+/* Temp choice */
+
+function tempChoice() {
+    for (var i = 0; i < inputRadioCollection.length; i++) {
+        inputRadioCollection[i].addEventListener('change', function(){
+            tempChoiceOutput.innerHTML = `You want to choose: <b>${this.value}</b>`;
+        });
+    }
+}
+
+tempChoice()
+
+/* Show choice */
 
 showChoiceBtn.addEventListener('click', function() {
     for(var i = 0; i < inputRadioCollection.length; i++) {
